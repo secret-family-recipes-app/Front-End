9f6ac4b170dfd7fef8877c05da5de456b78a1405
sofialevin <slevin@M-4876.local>
2019-08-21 22:05:48 -0700

    Completed validation for username and password.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -5,15 +5,15 @@ import { Button, Form } from 'semantic-ui-react'
 
 const loginData = [
     {
-        "username": "sofia",
-        "password": "12345"
+        username: "sofia",
+        password: "12345"
     }
 ]
 
-function LogIn() {
+function LogIn(props) {
     const [userData, setUserData] = useState({
-        "username": "",
-        "password": ""
+        username: "",
+        password: ""
     });
 
     const handleChange = event => {
@@ -22,11 +22,23 @@ function LogIn() {
         });
       }
 
-      console.log(userData);
+    const handleSubmit = event => {
+        event.preventDefault();
+        const currentUser = loginData.filter(user => (user.username === userData.username))
+            if (currentUser.length < 1) {
+                console.log("User does not exist.");
+            } else if (currentUser.length === 1 && currentUser[0].password !== userData.password) {
+                console.log("Wrong password");
+            } else {
+                console.log("Success!");
+            }
+        }
+
+console.log(userData);
 
   return (
     <div>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Field>
                 <label>Username</label>
                 <input placeholder='Username' name="username" value={userData.username} onChange={handleChange}/>
