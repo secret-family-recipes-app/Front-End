4482e178652d18b3b8b1f0d017acba966964b549
sofialevin <slevin@M-4876.local>
2019-08-21 20:28:24 -0700

    Set basic user data state and a data array for testing.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'semantic-ui-react'
 
+// test login data
+
+const loginData = [
+    {
+        "username": "sofia",
+        "password": "12345"
+    }
+]
+
 function LogIn() {
+    const [userData, setUserData] = useState({
+        "username": "",
+        "password": ""
+    });
+
+    const handleChange = event => {
+        setUserData({...userData, 
+          [event.target.name]: event.target.value
+        });
+      }
+
+      console.log(userData);
 
   return (
     <div>
         <Form>
             <Form.Field>
-                <label>First Name</label>
-                <input placeholder='First Name' />
+                <label>Username</label>
+                <input placeholder='Username' name="username" value={userData.username} onChange={handleChange}/>
             </Form.Field>
             <Form.Field>
-                <label>Last Name</label>
-                <input placeholder='Last Name' />
+                <label>Password</label>
+                <input placeholder='Password' name="password" value={userData.password} onChange={handleChange}/>
             </Form.Field>
-            <Button type='submit'>Submit</Button>
+            <Button type='submit'>Log In</Button>
         </Form>
     </div>
   );
