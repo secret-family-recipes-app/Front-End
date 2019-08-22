7655f7353132fd8afee520109dfecb76368d9760
sofialevin <slevin@M-4876.local>
2019-08-21 23:16:12 -0700

    Linked authenticated state to localStorage.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import LogIn from "./components/LogIn";
 import { Menu } from 'semantic-ui-react'
 
 function App() {
   const [userAuth, setUserAuth] = useState({
-    isAuthenticated: false
+    isAuthenticated: localStorage.getItem('authenticated')
   });
 
-const userHasAuthenticated = authenticated => {
-  setUserAuth({ isAuthenticated: authenticated })
-}
+  const userHasAuthenticated = authenticated => {
+    setUserAuth({ isAuthenticated: authenticated })
+  }
+
+  useEffect(() => {
+    localStorage.setItem('authenticated', userAuth.isAuthenticated);
+  }, [userAuth.isAuthenticated]);
 
 const handleLogout = event => {
   userHasAuthenticated(false);
@@ -32,7 +36,7 @@ console.log(userAuth);
       : <Menu secondary>
       </Menu>
       }
-      <LogIn userHasAuthenticated={userHasAuthenticated}/>
+      <LogIn userHasAuthenticated={userHasAuthenticated} userAuth={userAuth}/>
     </div>
   );
 }
